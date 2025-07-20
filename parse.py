81295d894b4fc29f2dce19046a9127c905a79175
GabrielVGS <gabrielviana3662@gmail.com>
2025-07-19 23:02:16 -0300

    first commit

---
+++ parse.py
@@ -0,0 +1,24 @@
+import json
+from glob import glob
+
+MEDICOES = sorted(glob("medicoes/*.json"))
+print(MEDICOES)
+
+
+
+def open_file(file_path: list[str]):
+    
+    for med in file_path:
+        json_correct = []
+        with open(med) as f:
+            for line in f:
+                l = json.loads(line)
+                json_correct.append(l)
+                print(json_correct)
+        
+        with open(f"correct_{med.split("/")[-1]}","w") as f:
+            json.dump(json_correct,f,indent=2)
+    ...
+    
+    
+open_file(MEDICOES)
